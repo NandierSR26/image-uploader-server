0ef10a07e93897b5c1b684e600e4510a6380b54a
Nandier Ruiz <nandiersruiz@outlook.com>
2022-03-31 21:15:46 -0500

    cors updated

--- models/Server.js
+++ models/Server.js
@@ -18,7 +18,7 @@ class Server {
 
     middlewares(){
         // CORS
-        this.app.use(cors(corsOptions))
+        this.app.use(cors())
 
         // parseo y lectura del body
         this.app.use(express.json())
@@ -44,18 +44,6 @@ class Server {
         })
     }
 
-}
-
-const whiteList = ['http://localhost:3000'];
-const corsOptions = {
-    origin: function (origin, callback) {
-        const existe = whiteList.some(dominio => dominio === origin);
-        if (existe) {
-            callback(null, true);
-        } else {
-            callback(new Error('No permitido por CORS'));
-        }
-    }
 }
 
 module.exports = Server;
