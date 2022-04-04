abc14dc4214cb41fe044c870ec33c2d4e0730aca
Nandier Ruiz <nandiersruiz@outlook.com>
2022-04-04 18:39:26 -0500

    settings

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-const Server = require('./models/Server');
-
+const express = require('express')
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const router = require('./routes');
 require('dotenv').config()
 
-const server  = new Server();
+const app = express();
 
-server.listen();
+const whitelist = [process.env.FRONTEND_URL];
+const corsOptions = {
+    origin: function (origin, callback) {
+        const existe = whitelist.some(dominio => dominio === origin);
+        if (existe) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    }
+}
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use( express.static('public') );
+
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : '/tmp/',
+    createParentPath : true
+}))
+
+app.use(router);
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Servidor correindo en el puerto ', process.env.PORT);
+})
--- models/Server.js
+++ models/Server.js
@@ -1,49 +1,47 @@
-const express = require('express')
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
 
-class Server {
-    constructor() {
-        this.app = express()
-        this.port = process.env.PORT;
 
-        this.paths = {
-            uploads: '/'
-        }
+// class Server {
+//     constructor() {
+//         this.app = express()
+//         this.port = process.env.PORT;
 
-        this.middlewares();
+//         this.paths = {
+//             uploads: '/'
+//         }
 
-        this.routes()
-    }
+//         this.middlewares();
 
-    middlewares(){
-        // CORS
-        this.app.use(cors())
+//         this.routes()
+//     }
 
-        // parseo y lectura del body
-        this.app.use(express.json())
+//     middlewares(){
+//         // CORS
+//         this.app.use(cors())
 
-        // directorio publico
-        this.app.use( express.static('public') );
+//         // parseo y lectura del body
+//         this.app.use(express.json())
 
-        // carga de archivos
-        this.app.use(fileUpload({
-            useTempFiles : true,
-            tempFileDir : '/tmp/',
-            createParentPath : true
-        }));
-    }
+//         // directorio publico
+//         this.app.use( express.static('public') );
 
-    routes(){
-        this.app.use(this.paths.uploads, require('../routes/index'));
-    }
+//         // carga de archivos
+//         this.app.use(fileUpload({
+//             useTempFiles : true,
+//             tempFileDir : '/tmp/',
+//             createParentPath : true
+//         }));
+//     }
 
-    listen(){
-        this.app.listen(this.port, () => {
-            console.log('Servidor correindo en el puerto ', this.port);
-        })
-    }
+//     routes(){
+//         this.app.use(this.paths.uploads, require('../routes/index'));
+//     }
 
-}
+//     listen(){
+//         this.app.listen(this.port, () => {
+//             console.log('Servidor correindo en el puerto ', this.port);
+//         })
+//     }
 
-module.exports = Server;
+// }
+
+// module.exports = Server;
